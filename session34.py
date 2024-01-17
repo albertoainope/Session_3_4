9c37af903be5a92b586dcf4767d551f5f5ba0f37
AlbertoI <albertoinope@gmail.com>
2024-01-17 16:09:47 +0100

    Added an extra print line

--- session34.py
+++ session34.py
@@ -1,3 +1,7 @@
 #VCS --> Enable Version control and select Git. The python file should now be red.
+#Git --> Commit. The python file should now be green.
+#Git -> GitHub --> Share project on GitHub. Finally, the file is in blue.
+#Blue means that the file is tracked, but it has changed.
 
 print("hello world again")
+print("hello world again! this is a new change")
