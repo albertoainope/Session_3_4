f2400948474c36c7c3b62314dabc957b10e70022
AlbertoI <albertoinope@gmail.com>
2024-01-17 18:35:15 +0100

    Added an extra print line

--- session34.py
+++ session34.py
@@ -2,6 +2,32 @@
 #Git --> Commit. The python file should now be green.
 #Git -> GitHub --> Share project on GitHub. Finally, the file is in blue.
 #Blue means that the file is tracked, but it has changed.
+#Local and online/master repositories.
+
+#Computer architecture --> CPU (brute-force method, which is endless)
+#CPU is like the brains (executor), and closely linked to the memory (storage for the CPU).
+#Speed measured in hertz (inverse of a second) --> actions per second.
+#Frequency is 5 hertz, so 1 action is done in 0.2 seconds.
+#Chess cannot be solved by a CPU despite its insane speed.
+#I/O Devices for input (keyboard, mouse, mic) and output (monitor, sound card)
+#Algortihm is a recipe from a cookbook. Unambiguous specification of how to solve a class of problems.
+#ARM --> A compiler translates into the machine code for the specific architecture.
+#Sieve of Eratosthenes --> To find all the prime numbers less than or equal to a given integer n by Erastosthenes' method.
 
 print("hello world again")
 print("hello world again! this is a new change")
+
+#determine the list of prime numbers between 1 andd 100
+student = "Alberto"
+print(type(student))
+
+print(5/5)
+print(17%10)
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a, b, id(a), id(b))
+print(a is b)
+a.append(4)
+
+print(a)
